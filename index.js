237670eb2905b5bb0bbaaef30f37210b2587c98e
Rathan Kasam <rathanreddykasam@gmail.com>
2024-02-26 09:26:07 -0600

    pretiffier changes

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 
 //JSX => HTML-Like sytax or XML-Like syntax
 //JSX (transpiled before it reaches to JS) - PARCEL -> Babel
+
 const Title = () => (
-	<h1 className='heading1'>Namaste React is here using JSX! 🚀</h1>
+	<h1 className='heading'>Namaste React is here using JSX! 🚀</h1>
 );
 
 //Component composition => putting an element into another element
@@ -14,6 +15,7 @@ const HeaderComponent = () => (
 		<h1 className='heading'>This is a functional component.</h1>
 	</div>
 );
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<HeaderComponent />);
